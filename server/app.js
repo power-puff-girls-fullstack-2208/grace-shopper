d70c8fe83e1ad536fdad79f7245947c07c3e0d97
Eve Cho <e.young.echo@gmail.com>
2022-09-20 19:16:50 -0400

    deployment: app.js incorporated, confirmed works on localhost, Procfile added again to serve express on Heroku

--- server/app.js
+++ server/app.js
@@ -4,15 +4,20 @@ const cors = require('cors');
 const app = express();
 const volleyball = require('volleyball');
 const PORT = process.env.PORT || 3000;
+
+// commented some of these lines out because localhost did not like some of them
+// but as we develop more we'll end up uncommenting and they'll be relevant
+// -- Eve
+
 // static middleware
-app.use(volleyball);
-app.use(cors);
+// app.use(volleyball);
+// app.use(cors);
 //this is where some things should go
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use("/api", require("./api"));
-app.use("/", (req, res) => {
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use("/api", require("./api"));
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "/public/index.html"));
 });
-app.listen(PORT, ()=> console.log(`listening on port ${PORT}`));
+app.listen(PORT, ()=> console.log(`listening on port ${PORT}\ngo --> http://localhost:3000/`));
 module.exports = app;
