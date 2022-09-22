c57a5f9ab040d50fb075eb5fba0f814152e45f37
Eve Cho <e.young.echo@gmail.com>
2022-09-22 12:54:09 -0400

    feature: <Provider> imported from react-redux to support global store

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom";
 const Nav = () => {
     return (
         <div id="navBar">
-            <Link to='#'>Home (this will eventually be a logo/icon instead)</Link>
+            <Link to='/'>Home (this will eventually be a logo/icon instead)</Link>
             <ul>
                 <li>
-                    <Link to='#'>All Cards</Link>
+                    <Link to='/all'>All Cards</Link>
                 </li>
                 <li><Link to='#'>Type</Link>
                     <ul>
@@ -24,7 +24,7 @@ const Nav = () => {
                         <li><Link to='#'>Diamond and Pearl</Link></li>
                     </ul>
                 </li>
-                <li><Link to='#'>User</Link></li>
+                <li><Link to='/user'>User</Link></li>
                 <li><Link to='#'>Cart</Link></li>
             </ul>
         </div>
