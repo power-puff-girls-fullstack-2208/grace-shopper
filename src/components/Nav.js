d00b3c488bcea5e39092a3b208942ea2458d706a
Eve Cho <e.young.echo@gmail.com>
2022-09-21 20:10:03 -0400

    git

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,29 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import styled from 'styled-components';
+// package to deal with styling by component
+
+const NavBar = styled.div`
+    padding: 8px;
+    text-align: center;
+    width: 100vw;
+    position: sticky;
+    top: 0px;
+
+    a {
+        color: black;
+        margin: 5px;
+    }
+      
+    a:hover {
+        color: maroon;
+        margin: 5px;
+    }
+`
 
 const Nav = () => {
     return (
-        <div id="navBar">
+        <NavBar>
             <h1>This is the navbar placeholder</h1>
             <Link to='#'>Home</Link>
             <Link to='#'>All Cards</Link>
@@ -11,7 +31,7 @@ const Nav = () => {
             <Link to='#'>Generation</Link>
             <Link to='#'>User</Link>
             <Link to='#'>Cart</Link>
-        </div>
+        </NavBar>
     )
 }
 
