418474e4d3591713ee1906b21aa17f1e9c468d9d
Eve Cho <e.young.echo@gmail.com>
2022-09-22 12:49:45 -0400

    feature: components imported to App.js, routes added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter, Routes, Route, Link, Switch } from "react-router-dom";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
 import Nav from "./components/Nav";
+import AllProducts from "./components/AllProducts";
+import SingleProduct from "./components/SingleProduct";
+import User from "./components/User";
 
 const App = () => {
     return (
@@ -12,7 +15,9 @@ const App = () => {
                 <main>
                     <Routes>
                         <Route exact path='/' element={<Home/>}/>
-                        <Route path='/#' element={<br/>/* <Example/> */ }/>
+                        <Route path='/all' element={<AllProducts/>/* <Example/> */ }/>
+                        <Route path='/product' element={<SingleProduct/>/* <Example/> */ }/>
+                        <Route path='/user' element={<User/>/* <Example/> */ }/>
                         {/* add route for your component when you are ready to test it out */}
                     </Routes>
                 </main>
