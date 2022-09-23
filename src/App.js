83e14fc15e4ce0343c1465ac886d4e61c77d39a5
Eve Cho <e.young.echo@gmail.com>
2022-09-23 16:37:14 -0400

    cleanup: routes reordered in precedence

--- src/App.js
+++ src/App.js
@@ -20,16 +20,12 @@ const App = () => {
                 <main>
                     <Routes>
                         <Route exact path='/' element={<Home/>}/>
-                        {/* route that leads to ALL the cards */}
                         <Route path= '/login' element={<Login />} />
                         <Route path= '/register' element={<Register />} />
-                        {/* route that leads to all the cards, but filters in cards of a certain type from the nav dropdown */}
                         <Route path='/products/type/:type' element={<AllProducts/>}/>
-                        <Route path='/products' element={<AllProducts/>}/>
-                        <Route path='/products/:id' element={<SingleProduct/>}/>
-                        {/* route that leads to all the cards, but filters in cards of a certain rarity from the nav dropdown */}
                         <Route path='/products/rarity/:rarity' element={<AllProducts/>}/>
-                        {/* route that goes to a view single card given its id */}
+                        <Route path='/products/:id' element={<SingleProduct/>}/>
+                        <Route path='/products' element={<AllProducts/>}/>
                         <Route path= '/users' element={<User />} />
                         <Route path='/#' element={<br/>/* <Example/> */ }/>
                         
