ec741c5ffa5103a29cdbf797b8d139ee83ae573f
Eve Cho <e.young.echo@gmail.com>
2022-09-24 17:07:13 -0400

    bug: site is not showing updated code, at all

--- src/components/AllProducts.js
+++ src/components/AllProducts.js
@@ -17,6 +17,30 @@ const AllProducts = () => {
 
     return (
         <div className = 'productsContainer content'>
+            <h1>aaaaaaa</h1>
+            <div className='filtersPrompt'>
+                <button id='filterButton'>Show filtering options</button>
+            </div>
+            <div className='filters-overlay'>
+                <form id='filters-form'>
+                    <div id='poke-type'>
+                        <input type='checkbox' id='poke-type-type' name='type' value='true'></input>
+                        <label for='poke-type-type'>TYPE</label>
+                    </div>
+                    <div id='poke-rarity'>
+                        <input type='checkbox' id='poke-type-type' name='type' value='true'></input>
+                        <label for='poke-type-type'>TYPE</label>
+                    </div>
+                    <div id='poke-sets'>
+                        <input type='checkbox' id='poke-type-type' name='type' value='true'></input>
+                        <label for='poke-type-type'>TYPE</label>
+                    </div>
+                    <div id='filter-buttons'>
+                        <button onClick={() => {}}>Cancel</button>
+                        <button type='submit' onClick={() => console.log('submitted!')}>Apply</button>
+                    </div>
+                </form>
+            </div>
             <div className="contentContainer">
                 {products && products.length ? products.map((product) =>
                 <ViewCard card={product} key={product.id}/>
