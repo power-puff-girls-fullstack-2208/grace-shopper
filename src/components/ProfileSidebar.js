f373363c9d7ac8127e6e7b3161aec2c3013c90b2
Rahlien <rogerrabbit122@gmail.com>
2022-09-29 09:52:32 -0400

    changed css a bit, merged main, removed cookies because of breakage

---
+++ src/components/ProfileSidebar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function ProfileSidebar() {
+    return (
+        <div className="sidebar">
+            <ul>
+                <li>General Information</li>
+                <li>Edit User Info</li>
+                <li>Order History</li>
+            </ul>
+        </div>
+    )
+}
+
+export default ProfileSidebar
