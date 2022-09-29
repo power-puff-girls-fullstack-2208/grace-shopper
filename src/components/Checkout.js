9e0fe8edaec605f0cc042841acfad7f82b823ec2
Eve Cho <e.young.echo@gmail.com>
2022-09-29 10:23:28 -0400

    kermit

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React from 'react';
+import kermit from '../../public/kermit.jpg';
 
 export default function Checkout() {
   return (
-    <div className='content'>Congrats you have check out!</div>
+    <div className='content'>Congrats you have check out!
+      <img src={kermit} alt='kermit the frog' width='100%'/>
+    </div>
   )
 }
